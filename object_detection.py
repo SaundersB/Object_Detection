35e1b61e97b93a895ded1aa33364d0e3cffe510d
SaundersB <speakeasys@gmail.com>
2016-10-23 15:22:05 -0700

    Adding a greyscale conversion function

--- object_detection.py
+++ object_detection.py
@@ -46,6 +46,23 @@ def display_image(img):
 		except cv2.error as e:
 			print(e)
 
+def convert_to_grayscale(img):
+	gray cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	laplacian = cv2.Laplacian(img,cv2.CV_64F)
+	sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)  # x
+	sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)  # y
+
+	plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+	plt.title('Original'), plt.xticks([]), plt.yticks([])
+	plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+	plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+	plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+	plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+	plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+	plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+	plt.show()
+
 
 def plot_image(img):
 	plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
@@ -63,7 +80,8 @@ def capture_video():
 	capture.close()
 
 if __name__ == '__main__':
-	#print(find_files())
-	#img = read_image()
+	print(find_files())
+	img = read_image()
 	#display_image(img)
-	obtain_rtsp_stream()
+	convert_to_grayscale(img)
+	#obtain_rtsp_stream()
